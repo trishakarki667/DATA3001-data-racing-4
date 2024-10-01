d5f162b023a5daf1730a9fe99a9a66226bd58660
akonowoah <z5412992@ad.unsw.edu.au>
2024-10-01 10:32:59 +1000

    test

--- main code.py
+++ main code.py
@@ -3,6 +3,5 @@ import pandas as pd
 
 # Load the data from the CSV file you provided
 data_2022 = pd.read_csv("f1sim-data-2022.csv")  # Update with the correct file path
-# fdakfjdaskljfkdasjfj;aslj
 
-# test for chri
+# testing 
